8cf71e868eb17451460df1860d2a491c444efe19
Daniel <daniel0ar@hotmail.com>
2023-09-11 19:43:35 -0500

    feat: Add footer

--- public/src/app/page.jsx
+++ public/src/app/page.jsx
@@ -1,9 +1,11 @@
+import Footer from "airbnb/components/footer/Footer";
 import Navbar from "airbnb/components/navbar/Navbar";
 import React from "react";
 
 const page = () => {
   return <div>
     <Navbar></Navbar>
+    <Footer></Footer>
     </div>;
 };
 
--- public/src/components/footer/Footer.jsx
+++ public/src/components/footer/Footer.jsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
 import React from "react";
+import { FiGlobe } from "react-icons/fi";
+import { PiCaretUpBold } from "react-icons/pi";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  
+  const links = ["privacy", "terms", "sitemap", "company", "destinations"];
+
+  const renderedLinks = links.map((link) => {
+    return <li key={link}><Link href='#' className="capitalize">{link}</Link></li>
+  })
+
+  return (
+    <div className="px-20 border-t border-t-gray-200 py-4 flex justify-between w-full text-sm z-50 bg-white items-center">
+      <ul className="flex gap-3 font-normal">
+        <li>&copy; 2023 D3Properties</li>
+        {renderedLinks}
+      </ul>
+      <ul className="flex gap-4 font-medium">
+        <li className="flex items-center gap-2 cursor-pointer">
+          <FiGlobe></FiGlobe>English
+        </li>
+        <li className="cursor-pointer">$ USD</li>
+        <li className="flex items-center gap-2 cursor-pointer">Support & Resources <PiCaretUpBold></PiCaretUpBold></li>
+      </ul>
+    </div>
+  );
 }
