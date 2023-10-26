a48981756495c16af7b4af12528db7d7fa9c2a2a
Daniel <daniel0ar@hotmail.com>
2023-10-26 16:25:30 -0500

    new-listing(feat): Add step 14 (last)

--- public/src/components/process/ListingCreated.jsx
+++ public/src/components/process/ListingCreated.jsx
@@ -1,7 +1,35 @@
+import { useRouter } from "next/navigation";
 import React from "react";
+import Confetti from "react-confetti";
 
 const ListingCreated = () => {
-  return <div>ListingCreated</div>;
+
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center h-full">
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <h2 className="font-semibold text-4xl">Great!</h2>
+        <p>You have succcesfully created your listing!</p>
+        <div className="flex gap-5">
+          <button className="bg-d3prop-light-black py-3 mt-5 px-10 text-white hover:bg-gray-700 text-base font-medium rounded-md"
+            onClick={() => router.push('/')}>
+            Visit Home Page
+          </button>
+          <button className="bg-d3prop-gradient py-3 mt-5 px-5 text-white text-base font-medium rounded-md hover:bg-d3prop-gradient-alt"
+            onClick={() => router.push('/my-listings')}>
+            View your Listings
+          </button>
+        </div>
+        <Confetti 
+          width={window.innerWidth} 
+          height={window.innerHeight}
+          numberOfPieces={200}
+          recycle={false}
+          tweenDuration={15000}></Confetti>
+      </div>
+    </div>
+  );
 };
 
 export default ListingCreated;
