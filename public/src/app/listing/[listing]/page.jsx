e95c384af1b900110ab0476be39867e2213ebddb
Daniel <daniel0ar@hotmail.com>
2023-11-21 00:15:23 -0500

    listing(design): Responsive component corrections

--- public/src/app/listing/[listing]/page.jsx
+++ public/src/app/listing/[listing]/page.jsx
@@ -5,15 +5,17 @@ const Navbar = dynamic(() => import("airbnb/components/navbar/Navbar"), {
 });
 import { getListing } from "airbnb/lib/lisitng";
 import { useAppStore } from "airbnb/store/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ListingPhotos from "./components/ListingPhotos";
 import ListingAmenties from "./components/ListingAmenties";
 import ListingMap from "./components/ListingMap";
 import TripScheduler from "./components/TripScheduler";
 import Footer from "airbnb/components/footer/Footer";
+import { IoMdClose } from "react-icons/io";
 
 const page = ({ params: { listing } }) => {
   const { currentListing, setCurrentListing } = useAppStore();
+  const [isSchedulerOpen, setIsSchedulerOpen] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -31,15 +33,21 @@ const page = ({ params: { listing } }) => {
       <div>
         <Navbar></Navbar>
         <div
-          className="px-20 pt-10 text-d3prop-light-black grid gap-10"
+          className="px-8 md:px-20 pt-10 text-d3prop-light-black grid gap-10"
           style={{ gridTemplateColumns: "70fr 30fr" }}
         >
           <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-1">
               <h2 className="text-5xl">{currentListing.title}</h2>
-              <span className="text-lg cursor-pointer underline">
-                {currentListing.locationData.landmark}
+              <span className="text-lg underline">
+                in {currentListing.locationData.landmark}
               </span>
+              <button
+                className="md:hidden bg-d3prop-gradient py-3 mt-5 px-5 text-white text-base font-medium rounded-md cursor-pointer w-full"
+                onClick={() => setIsSchedulerOpen(true)}
+              >
+                Reserve
+              </button>
             </div>
             <ListingPhotos></ListingPhotos>
             <div className="flex flex-col gap-5">
@@ -68,9 +76,30 @@ const page = ({ params: { listing } }) => {
               </div>
             </div>
           </div>
-          <div>
+          <div className="hidden md:inline-block">
             <TripScheduler></TripScheduler>
           </div>
+          {isSchedulerOpen && (
+            <div className="md:hidden justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+              <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-[600px] lg:h-auto md:h-auto bg-white">
+                <div
+                  className="flex items-center p-6 rounded-t justify-center relative"
+                >
+                  <div
+                    className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+                  >
+                    <button
+                      className="p-1 hover:opacity-70 absolute z-10"
+                      onClick={() => setIsSchedulerOpen(false)}
+                    >
+                      <IoMdClose size={24} />
+                    </button>
+                    <TripScheduler></TripScheduler>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
         <Footer></Footer>
       </div>
--- public/src/components/views/ViewSwitchBadge.jsx
+++ public/src/components/views/ViewSwitchBadge.jsx
@@ -8,7 +8,7 @@ const ViewSwitchBadge = () => {
 
   return (
     <div
-      className="fixed flex justify-center items-center bottom-16 left-0 right-0 cursor-pointer"
+      className="fixed z-10 flex justify-center items-center bottom-24 left-0 right-0 cursor-pointer"
       onClick={() => setIsMapView()}
     >
       <div className="bg-black p-4 text-white rounded-full">
